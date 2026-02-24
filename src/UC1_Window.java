738bc9c630429d60315e81bfb1876a4ec80780f3
Rkncodes <your-rajvinderkaurpersonal@gmail.com>
2026-02-24 15:50:06 +0530

    UC1: Initial Window Setup

---
+++ src/UC1_Window.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class UC1_Window {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("UC1: Window Setup");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
